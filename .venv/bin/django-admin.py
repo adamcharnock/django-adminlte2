f4290643467f2343e4bb47b6cae551dc6f1d076e
Perceu Bertoletti <perceu.bertoletti@gmail.com>
2019-12-24 06:30:25 -0300

    ajustes na change list e no pesquisar default

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/perceu/Sources/django-adminlte2/.venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ .venv/lib/python3.7/site-packages/debug_toolbar/management/commands/debugsqlshell.py
@@ -0,0 +1,23 @@
+from time import time
+
+import sqlparse
+from django.core.management.commands.shell import Command  # noqa
+from django.db.backends import utils as db_backends_utils
+
+# 'debugsqlshell' is the same as the 'shell'.
+
+
+class PrintQueryWrapper(db_backends_utils.CursorDebugWrapper):
+    def execute(self, sql, params=()):
+        start_time = time()
+        try:
+            return self.cursor.execute(sql, params)
+        finally:
+            raw_sql = self.db.ops.last_executed_query(self.cursor, sql, params)
+            end_time = time()
+            duration = (end_time - start_time) * 1000
+            formatted_sql = sqlparse.format(raw_sql, reindent=True)
+            print("{} [{:.2f}ms]".format(formatted_sql, duration))
+
+
+db_backends_utils.CursorDebugWrapper = PrintQueryWrapper
